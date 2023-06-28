f41c332b357955c9234abf1e345e0d816fe1d8f9
Tiffany Bissonnette <tiffanybissonnette@Tiffanys-Air.askey.com>
2023-06-28 01:40:04 -0400

    more

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,92 +7,93 @@ import attraction from '../images/attraction3.webp'
 import waterfall from '../images/waterfall2.jpg'
 import UserContext from '../context/UserContext';
 import beach from "../images/beach.jpeg";
+import { useAnimate, usePresence, motion } from 'framer-motion';
 
 
 function Home() {
-    const [data, setData] = useState([{}])
-    const {user} = useContext(UserContext)
+  const [data, setData] = useState([{}])
+  const { user } = useContext(UserContext)
 
-    useEffect (() => {
-      fetch("/location/api").then(
-        response => response.json()
-      ).then(
-        data => {
-          setData(data)
-          console.log(data)
-        }
-      )
-    }, [])
-    return (
-
-        <div className='home-container'>
-        <div className='imgContainer mt-10'>
-            <img className = "homeImg" src={waterfall} />
-            </div>
-            <div className="homeContainer card w-96 shadow-sm">
-                    <h2 className="exploreTitle card-title text-center">Adventure Awaits</h2>
-                    <p className='exploreText text-center'>Let's plan your next journey together!</p>
-                    <div className="exploreButton card-actions justify-end">
-                        <a href="#homeHeader" className='smooth'><button className="btn btn-primary">Explore</button></a>
-                </div>
-    </div>
-            <h1 className='homeHeader font-semibold' id='homeHeader'>Plan ahead to minimize stress and maximize your vacation!</h1>
-            <h2 className='homeSub text-center pb-7'>Filter by category and add what you like to your travel itinerary</h2>
-            <div class="grid-container">
-  <div className="Hotels card w-50 bg-base-100 shadow-xl">
-  <figure className='img'><img src={hotel} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="hotel-title card-title">Hotels</h2>
-    <div className="hotel-action card-actions justify-start">
-      <a href='/hotels'><button className="hotel-button btn btn-primary">Explore</button></a>
-    </div>
-  </div>
-  </div>
-  <div className="Vacation-Rentals card w-50 bg-base-100 shadow-xl">
-  <figure className='img'><img src={rental} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Vacation Rentals</h2>
-    <div className="card-actions justify-start">
-    <a href='/rentals'><button className="btn btn-primary">Explore</button></a>
-    </div>
-  </div>
-  </div>
-  <div className="Restaurants card w-50 bg-base-100 shadow-xl">
-  <figure className='beach-img img'><img src={food} alt="Shoes" /></figure>
-  <div className="joinus-body card-body">
-    <h2 className="card-title">Restaurants</h2>
-    <div className="card-actions justify-start">
-    <a href='/restaurants'><button className="btn btn-primary">Explore</button></a>
-    </div>
-  </div>
-  </div>
-  <div className="Attractions card w-50 bg-base-100 shadow-xl">
-  <figure className='img'><img src={attraction} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Attractions</h2>
-    <div className="card-actions justify-start">
-    <a href='/attractions'><button className="btn btn-primary">Explore</button></a>
-    </div>
-  </div>
-</div>
-</div>
-<div>
-          {!user.id &&
-         <div className="bottom-card card lg:card-side bg-base-100 shadow-xl">
-         <figure><img src={beach} alt="Album"/></figure>
-         <div className="card-body">
-           <h2 className="joinus card-title">Join us</h2>
-           <p>Sign up to gain access to features such as the itinerary and favorites to enhance your travel experience.</p>
-           <div className="card-actions justify-end">
-             <a href='/signup'><button className="btn btn-primary">Sign up</button></a>
-             <p className='signin-text text-sm'>Already have an account? <a className='link link-hover' href='/login'>Login</a></p>
-           </div>
-         </div>
-       </div>
-          }
-        </div>
-        </div>
+  useEffect(() => {
+    fetch("/location/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
     )
+  }, [])
+  return (
+
+    <div className='home-container'>
+      <div className='imgContainer mt-10'>
+        <img className="homeImg" src={waterfall} />
+      </div>
+      <div className="homeContainer card w-96 shadow-sm">
+        <h2 className="exploreTitle card-title text-center">Adventure Awaits</h2>
+        <p className='exploreText text-center'>Let's plan your next journey together!</p>
+        <div className="exploreButton card-actions justify-end">
+          <a href="#homeHeader" className='smooth'><button className="btn btn-primary">Explore</button></a>
+        </div>
+      </div>
+      <h1 className='homeHeader font-semibold' id='homeHeader'>Plan ahead to minimize stress and maximize your vacation!</h1>
+      <h2 className='homeSub text-center pb-7'>Filter by category and add what you like to your travel itinerary</h2>
+      <div class="grid-container">
+        <motion.div className="Hotels card w-50 bg-base-100 shadow-xl" initial={{opacity:0}} whileInView={{opacity:1}}>
+          <figure className='img'><img src={hotel} alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="hotel-title card-title">Hotels</h2>
+            <div className="hotel-action card-actions justify-start">
+              <a href='/hotels'><button className="hotel-button btn btn-primary">Explore</button></a>
+            </div>
+          </div>
+        </motion.div>
+        <motion.div className="Vacation-Rentals card w-50 bg-base-100 shadow-xl" initial={{opacity:0}} whileInView={{opacity:1}}>
+          <figure className='img'><img src={rental} alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Vacation Rentals</h2>
+            <div className="card-actions justify-start">
+              <a href='/rentals'><button className="btn btn-primary">Explore</button></a>
+            </div>
+          </div>
+        </motion.div>
+        <motion.div className="Restaurants card w-50 bg-base-100 shadow-xl" initial={{opacity:0}} whileInView={{opacity:1}}>
+          <figure className='beach-img img'><img src={food} alt="Shoes" /></figure>
+          <div className="joinus-body card-body">
+            <h2 className="card-title">Restaurants</h2>
+            <div className="card-actions justify-start">
+              <a href='/restaurants'><button className="btn btn-primary">Explore</button></a>
+            </div>
+          </div>
+        </motion.div>
+        <motion.div className="Attractions card w-50 bg-base-100 shadow-xl" initial={{opacity:0}} whileInView={{opacity:1}}>
+          <figure className='img'><img src={attraction} alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Attractions</h2>
+            <div className="card-actions justify-start">
+              <a href='/attractions'><button className="btn btn-primary">Explore</button></a>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+      <div>
+        {!user.id &&
+          <motion.div className="bottom-card card lg:card-side bg-base-100 shadow-xl" initial={{opacity:0}} whileInView={{opacity:1}}>
+            <figure><img src={beach} alt="Album" /></figure>
+            <div className="card-body">
+              <h2 className="joinus card-title">Join us</h2>
+              <p>Sign up to gain access to features such as the itinerary and favorites to enhance your travel experience.</p>
+              <div className="card-actions justify-end">
+                <a href='/signup'><button className="btn btn-primary">Sign up</button></a>
+                <p className='signin-text text-sm'>Already have an account? <a className='link link-hover' href='/login'>Login</a></p>
+              </div>
+            </div>
+          </motion.div>
+        }
+      </div>
+    </div >
+  )
 
 }
 export default Home
--- src/views/ViewHotels.js
+++ src/views/ViewHotels.js
@@ -1,102 +1,125 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import "./ViewHotels.css"
 
 const ViewHotels = () => {
-  let {hotelId} = useParams();
-  let {indate} = useParams();
-  let {outdate} = useParams()
+  let { hotelId } = useParams();
+  let { indate } = useParams();
+  let { outdate } = useParams()
 
   const [hotel, setHotel] = useState([])
-  async function hotelDetails(){
-    fetch('http://127.0.0.1:5000/hotel/details',{
+  async function hotelDetails() {
+    fetch('http://127.0.0.1:5000/hotel/details', {
       method: "POST",
       headers: {
         "Content-Type": 'application/json',
         "Access-Control-Allow-Origin": "*",
       },
-      body:JSON.stringify({
+      body: JSON.stringify({
         hotelId: hotelId,
         indate: indate,
         outdate: outdate,
       })
-    }).then(res=>{
+    }).then(res => {
       return res.json()
-    }).then (data=> {console.log(data?.data);
+    }).then(data => {
+      console.log(data?.data);
       const details = data?.data
       setHotel(details)
     }).catch(error => console.log('ERROR'))
   }
-  useEffect(()=>{
+  useEffect(() => {
     hotelDetails()
   }, [])
-  if(hotel===[]){
+  if (hotel === []) {
     return null
   }
   return (
     <div>
       <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img className="img-1"src={hotel?.photos?.[0].urlTemplate?.split('?')[0]+"?w=800&h=600&s=1"} />
-    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-1/2"> 
-      <a href="#slide2" className="btn btn-circle btn-primary">❯</a>
-    </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img className="img-2"src={hotel?.photos?.[1].urlTemplate?.split('?')[0]+"?w=800&h=600&s=1"} />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle btn-primary">❮</a> 
-      <a href="#slide3" className="btn btn-circle btn-primary">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img className="img-3"src={hotel?.photos?.[2].urlTemplate?.split('?')[0]+"?w=800&h=600&s=1"} />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle btn-primary">❮</a> 
-      <a href="#slide4" className="btn btn-circle btn-primary">❯</a>
-    </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img className="img-4"src={hotel?.photos?.[3].urlTemplate?.split('?')[0]+"?w=800&h=600&s=1"} />
-    <div className="absolute flex justify-start transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle btn-primary">❮</a> 
-    </div>
-  </div>
-</div>
-<div className="view-card card w-96 bg-base-100 shadow-xl">
-  <div className="card-body text-center">
-    <h2 className="card-title">{hotel.title}</h2>
-    <p>{hotel?.location?.address}</p>
-    <p className='amentities'>Amentities include:</p>
-    <p>{hotel?.amenitiesScreen?.[0].content[0]}</p>
+        <div id="slide1" className="carousel-item relative w-full">
+          <img className="img-1" src={hotel?.photos?.[0].urlTemplate?.split('?')[0] + "?w=800&h=600&s=1"} />
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle btn-primary">❯</a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img className="img-2" src={hotel?.photos?.[1].urlTemplate?.split('?')[0] + "?w=800&h=600&s=1"} />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle btn-primary">❮</a>
+            <a href="#slide3" className="btn btn-circle btn-primary">❯</a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img className="img-3" src={hotel?.photos?.[2].urlTemplate?.split('?')[0] + "?w=800&h=600&s=1"} />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle btn-primary">❮</a>
+            <a href="#slide4" className="btn btn-circle btn-primary">❯</a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img className="img-4" src={hotel?.photos?.[3].urlTemplate?.split('?')[0] + "?w=800&h=600&s=1"} />
+          <div className="absolute flex justify-start transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle btn-primary">❮</a>
+          </div>
+        </div>
+      </div>
+      <div className="view-card card w-96 bg-base-100 shadow-xl">
+        <div className="card-body text-center">
+          <h2 className="card-title">{hotel.title}</h2>
+          <p>{hotel?.location?.address}</p>
+          <p className='amentities'>Amentities include:</p>
+          <ul>
+            {hotel?.amenitiesScreen?.map(amenity => (
+              <li>{amenity?.content[0]}</li>
+            ))}
+          </ul>
+          {/* <p>{hotel?.amenitiesScreen?.[0].content[0]}</p>
     <p>{hotel?.amenitiesScreen?.[1].content[0]}</p>
     <p>{hotel?.amenitiesScreen?.[2].content[0]}</p>
     <p>{hotel?.amenitiesScreen?.[3].content[0]}</p>
-    <p>{hotel?.amenitiesScreen?.[4].content[0]}</p>
-    <p>{hotel.rating} <i class="fa-sharp fa-solid fa-star"></i> ({hotel?.reviews?.count})</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Add to Itinerary</button>
-    </div>
-  </div>
-</div>
-<div className='attractions'>
-  {hotel?.attractionsNearby?.content && hotel?.attractionsNearby?.content.map((a)=>{
-    return(
-      <div key={a.title} className="attraction card bg-base-100 shadow-xl">
-         <figure><img src={a?.cardPhoto?.urlTemplate?.split('?')[0]+'?w=600&h=300&s=1'} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">{a.title}</h2>
-    <p>{a.distance}</p>
-    <p>{a?.bubbleRating?.rating} <i class="fa-sharp fa-solid fa-star"></i></p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">View</button>
-    </div>
-  </div>
+    <p>{hotel?.amenitiesScreen?.[4].content[0]}</p> */}
+          <p>{hotel.rating} <i class="fa-sharp fa-solid fa-star"></i> ({hotel?.reviews?.count})</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Add to Itinerary</button>
+          </div>
+        </div>
       </div>
-    )
-  })}
-</div>
-    </div>
+      <div className='attractions'>
+        {hotel?.attractionsNearby?.content && hotel?.attractionsNearby?.content.map((a) => {
+          return (
+            <div key={a.title} className="attraction card bg-base-100 shadow-xl">
+              <figure><img src={a?.cardPhoto?.urlTemplate?.split('?')[0] + '?w=600&h=300&s=1'} alt="No photo" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">{a.title}</h2>
+                <p>{a.distance}</p>
+                <p>{a?.bubbleRating?.rating} <i class="fa-sharp fa-solid fa-star"></i> ({a?.bubbleRating?.numberReviews})</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">View</button>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+        </div>
+        <div className='restaurants'>
+          {hotel.restaurantsNearby?.content && hotel?.restaurantsNearby.content.map((r) => {
+            return (
+              <div key={r.title} className="restaurant card bg-base-100 shadow-xl">
+                <figure><img src={r?.cardPhoto?.urlTemplate?.split('?')[0] + '?w=600&h=300&s=1'} alt="No photo" /></figure>
+                <div className="card-body">
+                  <h2 className="card-title">{r.title}</h2>
+                  <p>{r.distance}</p>
+                  <p>{r?.bubbleRating?.rating} <i class="fa-sharp fa-solid fa-star"></i> ({r?.bubbleRating?.numberReviews})</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">View</button>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+            </div>
   )
 }
 
